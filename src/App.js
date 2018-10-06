53ec818154ce741f90670fa0a61810539cd35743
chandu <chandrasr.palle@gmail.com>
2018-10-06 14:20:27 +0200

    unused imports removed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Calendar from "./components/Calendar";
 import 'bootstrap/dist/css/bootstrap.min.css';
