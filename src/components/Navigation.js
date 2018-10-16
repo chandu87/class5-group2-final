6bea887dbf1c59a3c820cd7803bec812aa9591d2
chandu <chandrasr.palle@gmail.com>
2018-10-16 14:50:45 +0200

    navigation links updated

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,14 @@
 import React from "react";
 import logo from "../thread-logo.png";
 
+import { Link } from "react-router-dom";
+
 class Navigation extends React.Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container">
-          <a className="navbar-brand" href="#">
+          <a className="navbar-brand" href="/">
             <img src={logo} className="App-logo" alt="logo" />
           </a>
           <button
@@ -24,30 +26,20 @@ class Navigation extends React.Component {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Home 
-                </a>
+                <Link to="/" className="nav-link"> Home </Link>
+              </li>
+              
+              <li className="nav-item">
+                <Link to="/Internships" className="nav-link"> Internships </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Internships
-                </a>
-              </li>
-              <li className="nav-item active">
-                <a className="nav-link" href="#">
-                  Events
-                </a>
+                <Link to="/Events" className="nav-link"> Events </Link>  
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Mentors
-                </a>
+                <Link to="/Mentors" className="nav-link"> Mentors </Link>              
               </li>
-
               <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Networking
-                </a>
+                <Link to="/Networking" className="nav-link"> Networking </Link>              
               </li>
             </ul>
           </div>
