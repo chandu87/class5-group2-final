14d3da92394c5a8d6913896560f7a9a9ecd5b605
chandu <chandrasr.palle@gmail.com>
2018-10-16 13:45:16 +0200

    code refactored new folder created for each page

---
+++ src/components/internships/Internships.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Navigation from "../Navigation";
+
+class Internships extends React.Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <Navigation />
+        </header>
+        <main>
+          <li>Internships</li>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Internships;
---
+++ src/components/networking/Networking.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Navigation from "../Navigation";
+
+class Networking extends React.Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <Navigation />
+        </header>
+        <main>
+          <li>Networking</li>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Networking;
