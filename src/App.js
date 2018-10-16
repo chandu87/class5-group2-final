11e8b14372793997f34ef6b011c920c50d4f3945
chandu <chandrasr.palle@gmail.com>
2018-10-16 14:50:59 +0200

    unneccessary code removed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {Route, Switch } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
