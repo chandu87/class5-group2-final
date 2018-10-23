9268da16c83210f90c656fcefcfc907eb6a09d4b
chandu <chandrasr.palle@gmail.com>
2018-10-23 16:06:17 +0200

    webpack configuation changed

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -1,22 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-module.exports={
-    entry: [
-        './src/index.js'
-      ],
-      output:{
-        path:__dirname,
-        filename:'./public/bundle.js'
+const outputDirectory = path.join(__dirname, 'dist/client');
+
+module.exports = {
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  entry: './src/client/index.js',
+  output: {
+    path: outputDirectory,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
       },
-      module:{
-        loaders:[
-          {
-            loader:'babel-loader',
-            query:{
-              presets :['react', 'es2015','stage-0']
+      {
+        test: /\.css$/,
+        use: [
+            {
+              loader: 'style-loader',
             },
-            test:/\.js?$/,
-            exclude:/(node_modules)/
-          }
-        ]
-      }   
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+              }
+            },
+            {
+              loader: 'postcss-loader'
+            }
+          ],
+      },
+      {
+        test: /\.(png|jpg|JPG|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000',
+      },
+    ],
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': 'http://localhost:8080',
+    },
+    historyApiFallback: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin([outputDirectory]),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: './public/favicon.ico',
+    }),
+  ],
 };
