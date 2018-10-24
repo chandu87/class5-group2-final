420f5933c96bd2d49220ab0413def86bdfd51d78
chandu <chandrasr.palle@gmail.com>
2018-10-24 11:09:02 +0200

    express server endpoints added

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,24 @@
-const express = require('express');
-const app = express();
+import express from "express";
+import path from 'path';
+import bodyParser from 'body-parser';
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
-app.listen(5656, () => {
-    console.log('http://localhost:5656')
+import apiRoutes from './routes';
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/api', apiRoutes);
+
+// Serve the static files from the React app
+app.use(express.static(path.resolve(__dirname, '..', 'client')));
+
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'client', 'index.html'));
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
 });
