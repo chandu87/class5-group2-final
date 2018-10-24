7c24f8b5310c06b93fe8962ab9131b60f6001639
chandu <chandrasr.palle@gmail.com>
2018-10-24 15:22:01 +0200

    connection string changed for DB

--- src/server/config/db.js
+++ src/server/config/db.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2';
 
 const DATABASE_URL =
-  process.env.DATABASE_URL || 'mysql://hyf:password@127.0.0.1:3306/hyf_thread';
+  process.env.DATABASE_URL || 'mysql://bhgiry75if89y54y:z87wuynq5ru45x4d@e7qyahb3d90mletd.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/o15gaxnbvz5n269f';
 const connection = mysql.createConnection(DATABASE_URL);
 
 export default connection;
