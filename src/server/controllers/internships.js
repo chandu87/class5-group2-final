32fc68ef9589c12ae1e380fa11f2de89d8dbff52
chandu <chandrasr.palle@gmail.com>
2018-10-26 00:27:31 +0200

    sql string changed

--- src/server/controllers/internships.js
+++ src/server/controllers/internships.js
@@ -2,7 +2,7 @@ import SqlString from 'sqlstring';
 import db from '../config/db';
 
 export function listAllInternships(req, res) {
-  const sql = SqlString.format('SELECT * FROM internships WHERE is_active=?', [
+  const sql = SqlString.format('SELECT * FROM internships WHERE active=?', [
     true,
   ]);
   console.log(sql);
