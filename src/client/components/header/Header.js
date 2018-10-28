32759a8985c6c67265fb77917baf2ae4ab80a07f
majidkhan <majidkhan_l@hotmail.com>
2018-10-28 08:22:00 +0100

    adding container, row and col divs to Internships page

--- src/client/components/header/Header.js
+++ src/client/components/header/Header.js
@@ -10,12 +10,12 @@ class Header extends React.Component {
             <div className="container-fluid">
                 <div className="container">
                     <div className="row">
-                            <div className="col-12 col-sm-12 col-md-3 col-lg-3">
+                            <div className="col-12 col-sm-12 col-md-1 col-lg-3">
                                 <div className="siteLogo">
                                 <a href="#link"><img src={logo} width="50px" height="60px" alt="Thread Logo" /></a>
                                 </div>
                             </div>
-                            <div className="col-12 col-sm-12 col-md-9 col-lg-9">
+                            <div className="col-12 col-sm-12 col-md-11 col-lg-9">
                                 <div className="menu">
                                     <nav>
                                         <ul className="nav">
@@ -33,7 +33,7 @@ class Header extends React.Component {
                                                 <Link to="/Mentors" className="menuLink nav-link"> Mentors </Link></li>
                                             <li className="nav-item iconNetwork">
                                                 <span className="icon material-icons md-36 md-dark">people</span>
-                                                <Link to="/Networking" className=" nav-link"> Networking </Link></li>
+                                                <Link to="/Networking" className="nav-link"> Networking </Link></li>
                                         </ul>
                                     </nav>
                                 </div>
