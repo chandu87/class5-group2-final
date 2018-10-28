d71d16e84b055d68ec474c2ffcca7a4d37427855
Pankaj Kumar Singh <pankaj28843@gmail.com>
2018-10-28 15:59:04 +0100

    Fetch the mentor data before sending it to the form in the EditMentor component

--- src/client/components/mentor-form/edit-mentor.js
+++ src/client/components/mentor-form/edit-mentor.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MentorForm from './mentor-form';
 
-export default (props) => {
-    return (
-        <MentorForm {...props} isEditing={true} />
-    )
+// export default (props) => {
+//     // Do the fetching, and render the form only when the data is here
+//     return (
+//         <MentorForm {...props} isEditing={true} />
+//     )
+// }
+
+class EditMentor extends Component {
+    state = {
+        isLoading: true,
+        mentorData: null
+    }
+
+    componentDidMount() {
+        const url = '/api/mentors'
+        const id = this.props.match.params.id;
+
+        fetch(`${url}/${id}`)
+        .then(
+            response => response.json()
+        ).then(
+            data => this.setState({
+                isLoading: false,
+                mentorData: data
+            })
+        )
+    }
+
+    render() {
+        return (
+            this.state.isLoading ? 
+                <div>Hang in there...</div>
+                :
+                <MentorForm {...this.props} mentorData={this.state.mentorData}  isEditing={true} />
+        )
+    }
 }
+
+export default EditMentor;
