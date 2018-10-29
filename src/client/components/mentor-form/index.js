214a336c84d24f7a37069654371ddbbf4c654b7f
majidkhan <majidkhan_l@hotmail.com>
2018-10-29 20:51:31 +0100

    issue mergin Pankaj changes in local repo

---
+++ src/client/components/mentor-form/index.js
@@ -0,0 +1,11 @@
+// import MentorForm from './mentor-form';
+import AddMentor from './add-mentor';
+import EditMentor from './edit-mentor';
+
+console.log({ bla: AddMentor, bla2: EditMentor });
+
+export default {
+    // MentorForm,
+    AddMentor,
+    EditMentor,
+}
