e06f2529d9d86a983d433f8f8642262cb4a9909c
chandu <chandrasr.palle@gmail.com>
2018-10-31 17:41:04 +0100

    variable name changed for events map

--- src/client/components/events/EventsListView.js
+++ src/client/components/events/EventsListView.js
@@ -2,19 +2,23 @@ import React from 'react';
 
 class EventsListView extends React.Component{
     render(){
-        console.log(this.props.events);
+        // console.log(this.props.events);
         const events = this.props.events;
         return(
                 <div className="row">
                     <div className="list-group">
-                    {events.map(event =>  (
-                            <a key={event.id} href="#" className="list-group-item list-group-item-action flex-column align-items-start">
+                    {events.map(item =>  (
+                            <a key={item.id} href="#" className="list-group-item list-group-item-action flex-column align-items-start">
                                 <div className="d-flex w-100 justify-content-between">
-                                <h5 className="mb-1">{event.internship_title}</h5>
-                                <small>{event.internship_deadline}</small>
+                                <h5 className="mb-1">{item.event_name}</h5>
+                                <small>{item.event_language}</small>
                                 </div>
-                                <p className="mb-1">{event.internship_description}</p>
-                                <small>Category : {event.internship_category}</small>
+                                <p className="mb-1">{item.event_agenda}</p>
+                                <small>Category : {item.event_type}</small>
+                                <div>
+                                <small>Date : <strong>From</strong> {item.event_start_date} - {item.event_start_hour} 
+                                <strong> To </strong>{item.event_end_date} - {item.event_end_hour}</small>
+                                </div>
                             </a>
                             ))
                     }
