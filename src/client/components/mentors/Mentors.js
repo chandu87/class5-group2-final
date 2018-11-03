8d758153ee953db2bd6b1b20ef2c04ede6cc4018
majidkhan <majidkhan_l@hotmail.com>
2018-11-04 00:06:15 +0100

    Style Mentors page. Upload HackyourFuture images for mentors/events to MySQL DB for mentors page

--- src/client/components/mentors/Mentors.js
+++ src/client/components/mentors/Mentors.js
@@ -39,14 +39,13 @@ class Mentors extends React.Component {
           <div className="row">
           <div className="col">
           <h1>Mentors</h1> <br />
-            <a href="/Mentors/add">Add new</a>
+            <Link className="btn btn-outline-danger btn-lg btn-block mentor-add-button" to="/Mentors/add">Add New Mentor</Link>
             <div className="card-columns">
 
             {items.map(item => (
               <div key={item.id} class="card">
-              <img className="mentor-image" src={item.profile_picture} alt={item.first_name + " "+ item.last_name} />
+              <img className="card-img-top" src={item.profile_picture} alt={item.first_name + " "+ item.last_name} />
               <div className="card-body">
-                <Link to={`/Mentors/edit/${item.id}`} target="_blank">Edit</Link>
                 <h5 className="card-title">{item.first_name + " "+ item.last_name}</h5>
                 <p className="card-text"><strong>Services</strong><br />
                   <span>{item.offering}</span> <br />
@@ -54,6 +53,7 @@ class Mentors extends React.Component {
                   <span>{item.availability}</span> <br />
                 </p>
                 <a href="#" class="btn btn-danger">Read more..</a>
+                <Link to={`/Mentors/edit/${item.id}`} target="_blank" className="btn btn-outline-danger btn-sm pull-right"> Edit</Link>                
               </div>
               </div>
               ))}
