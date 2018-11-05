a972afa4289b1b45f37f1574d4bc24806ae93632
Ahmad Magdy <magdy.a@hotmail.com>
2018-11-06 00:44:53 +0100

    Removed an unused console.log in metor-form/index.js

--- src/client/components/mentor-form/index.js
+++ src/client/components/mentor-form/index.js
@@ -2,8 +2,6 @@
 import AddMentor from './add-mentor';
 import EditMentor from './edit-mentor';
 
-console.log({ bla: AddMentor, bla2: EditMentor });
-
 export default {
     // MentorForm,
     AddMentor,
