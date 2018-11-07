719af78175d82fa70d919609c8393b0aeb347789
majidkhan <majidkhan_l@hotmail.com>
2018-11-07 21:55:32 +0100

    modified magdy files for db as those were not required and make issues. implement login component mergin Magdy code. Login is implemented on Mentors component

--- scripts/start-mysql.sh
+++ scripts/start-mysql.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-
-docker run \
---rm \
--d \
--e MYSQL_ROOT_PASSWORD=password \
--e MYSQL_USER=hyf \
--e MYSQL_PASSWORD=password \
--p 3306:3306 \
--v $PWD/dump.sql:/docker-entrypoint-initdb.d/dump.sql \
-mysql:8.0.13 --default-authentication-plugin=mysql_native_password
+@@ -0,0 +1,11 @@
