57d511b06f47529f2d745caa220db940624dd6c3
majidkhan <majidkhan_l@hotmail.com>
2018-11-13 09:50:07 +0100

    create route of a detailed view of a mentor card

--- src/client/App.js
+++ src/client/App.js
@@ -6,27 +6,33 @@ import "./App.css";
 import "@material/card/dist/mdc.card.min.css";
 import 'react-notifications/lib/notifications.css';
 
-import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Homepage from "./components/home/HomePage";
+
 import Internships from "./components/internships/Internships";
 import InternshipItemDetails from "./components/internships/Internship-item-details";
 import InternshipForms from "./components/internship-form";
-import Homepage from "./components/home/HomePage";
+
 import Events from "./components/events/Events";
-import Mentors from "./components/mentors/Mentors";
-import Networking from "./components/networking/Networking";
-import NetworkingItemDetails from "./components/networking/Network-item-details";
-import Company from "./components/networking/Company";
-import MentorForms from "./components/mentor-form";
-import NetworkForms from "./components/networking-form";
 import EventsForm from './components/events-form';
 import EventDetailedView from "./components/events/EventDetailedView";
-import SearchBar from "./components/search-bar/SearchBar";
+
+import Mentors from "./components/mentors/Mentors";
+import MentorSearch from "./components/mentors/MentorSearch";
+import MentorForms from "./components/mentor-form";
+import MentorItemDetails from "./components/mentors/mentor-item-details";
+
+import Networking from "./components/networking/Networking";
+import NetworkingItemDetails from "./components/networking/Network-item-details";
+import NetworkForms from "./components/networking-form";
 import NetworkSearch from "./components/networking/NetworkSearch";
+
+import SearchBar from "./components/search-bar/SearchBar";
+
 import Login from "./components/login";
 import Logout from "./components/logout";
 import LoginContext, { loadContextValue } from './contexts/login';
-import MentorSearch from "./components/mentors/MentorSearch";
 
 class App extends Component {
   render() {
@@ -42,28 +48,33 @@ class App extends Component {
           <main>
             <SearchBar />
             <Switch>
+              { /* Home Page*/}
               <Route exact path="/" component={Homepage} />
+              { /* Internships Page*/}
               <Route exact path="/Internships" component={Internships} />
               <Route exact path="/Internships/add" component={InternshipForms.AddInternship} />
               <Route exact path="/Internships/edit/:id" component={InternshipForms.EditInternship} />
               <Route exact path="/Internships/delete/:id" component={InternshipForms.DeleteInternship}/>
               <Route exact path="/Internships/details/:id" component={InternshipItemDetails}/>
+              { /* Events Page*/}
               <Route exact path="/Events" component={Events} />
               <Route exact path="/Events/add" component={EventsForm.AddEvent} />
               <Route exact path="/Events/edit/:id" component={EventsForm.EditEvent} />
               <Route exact path="/Events/details/:id" component={EventDetailedView} />
+              { /* Mentors Page*/}
               <Route exact path="/Mentors" component={Mentors} />
               <Route exact path="/Mentors/add" component={MentorForms.AddMentor} />
-              <Route exact path={`/Mentors/edit/:id`} component={MentorForms.EditMentor} />
+              <Route exact path="/Mentors/edit/:id" component={MentorForms.EditMentor} />
+              <Route exact path="/Mentors/details/:id" component={MentorItemDetails}/>
+              <Route exact path="/Mentors/search" component={MentorSearch} />
+              { /* Networking Page*/}
               <Route exact path="/Networking" component={Networking} />
               <Route exact path="/Networking/add" component={NetworkForms.addNetwork}/>
               <Route exact path="/Networking/details/:id" component={NetworkingItemDetails}/>
               <Route exact path="/Networking/edit/:id" component={NetworkForms.editNetwork}/>
               <Route exact path="/Networking/delete/:id" component={NetworkForms.deleteNetwork}/>
-
-              {/* <Route exact path={`/Networking/company/:id`} component={Company} /> */}
               <Route exact path="/Networking/search" component={NetworkSearch} />
-              <Route exact path="/Mentors/search" component={MentorSearch} />
+              { /* Authentication Routes*/}
               <Route exact path="/login" component={Login} />
               <Route exact path="/logout" component={Logout} />
             </Switch>
