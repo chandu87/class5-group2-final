9ddc9843249f5824b0b73265f7b469c1a0743706
chandu <chandrasr.palle@gmail.com>
2018-11-24 16:50:34 +0100

    home page jumbotron modified

--- src/client/components/home/HomePage.js
+++ src/client/components/home/HomePage.js
@@ -4,29 +4,35 @@ class HomePage extends React.Component {
   render() {
     return (
       <div className="container-fluid home-page">
-                <div className="jumbotron">
-            <div className="container">
-            <h1 className="display-4">Welcome!</h1>
-            <p className="lead">
-              The goal for this project is to develop an online platform to
-              facilitate the connections between refugee women and individuals
-              or partners or other organisations which share an interest in
-              textiles and craft activity. The partners or other organisations
-              are part of the THREAD network therefore they will be metioned as
-              networking.
-            </p>
-            <hr className="my-4" />
-            <p>
-              THREAD stands for Textile Hub for Refugee Empowerment, Employment
-              & Entrepreneurship Advancement in Denmark THREAD is a dynamic and
-              innovative collaboration of diverse partners from business,
-              design, education, research and refugee support agencies aiming to
-              achieve life-changing results for women who are forging new lives
-              in Denmark.
-            </p>
-          </div>
-          </div>
+          <div className="jumbotron jumbo-image">
+          <div className="container">
 
+          
+<div className="jumbotron">
+
+  <h1 className="display-4">Welcome!</h1>
+  <p className="lead">
+    The goal for this project is to develop an online platform to
+    facilitate the connections between refugee women and individuals
+    or partners or other organisations which share an interest in
+    textiles and craft activity. The partners or other organisations
+    are part of the THREAD network therefore they will be metioned as
+    networking.
+  </p>
+  <hr className="my-4" />
+  <p>
+    THREAD stands for Textile Hub for Refugee Empowerment, Employment
+    & Entrepreneurship Advancement in Denmark THREAD is a dynamic and
+    innovative collaboration of diverse partners from business,
+    design, education, research and refugee support agencies aiming to
+    achieve life-changing results for women who are forging new lives
+    in Denmark.
+  </p>
+</div>
+
+</div>
+
+          </div>
       </div>
     );
   }
